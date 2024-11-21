cf85450199ae74e2ad30e338b4ab5bc866b989d9
uuon <uuon1029@mail.ulsan.ac.kr>
2024-11-22 01:32:56 +0900

    map order calender

---
+++ app/src/main/java/com/example/allgoing/Adapter/OrderCalendarRVAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.allgoing.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.allgoing.databinding.ItemOrderCalendarBinding
+
+class OrderCalendarRVAdapter(date_list: ArrayList<Int>): RecyclerView.Adapter<OrderCalendarRVAdapter.ViewHolder>() {
+
+    var date_list = date_list
+
+    fun interface MyClickListener {
+        fun itemSelect(posion:Int)
+    }
+
+    lateinit var myClickListener : MyClickListener
+
+    fun setClickListener(clickListener: MyClickListener){
+        myClickListener = clickListener
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemOrderCalendarBinding.inflate(LayoutInflater.from(viewGroup.context),viewGroup,false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+        holder.itemView.setOnClickListener {
+            myClickListener.itemSelect(position)
+        }
+    }
+
+    override fun getItemCount(): Int = date_list.size
+
+    inner class ViewHolder(val binding: ItemOrderCalendarBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(position: Int){
+            binding.itemOrderCalendarDate.text = date_list[position].toString()
+        }
+    }
+
+}
--- app/src/main/java/com/example/allgoing/fragment/DetailOrderFragment.kt
+++ app/src/main/java/com/example/allgoing/fragment/DetailOrderFragment.kt
@@ -1,15 +1,27 @@
 package com.example.allgoing.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.allgoing.Adapter.OrderCalendarRVAdapter
 import com.example.allgoing.databinding.FragmentDetailHomeBinding
 import com.example.allgoing.databinding.FragmentDetailOrderBinding
 
 class DetailOrderFragment : Fragment(){
     lateinit var binding: FragmentDetailOrderBinding
+
+    var date_list = arrayListOf(
+        0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +31,20 @@ class DetailOrderFragment : Fragment(){
         binding = FragmentDetailOrderBinding.inflate(inflater,container,false)
 
         return binding.root
+    }
+
+    private fun initRV() {
+
+        val calRVAdapter = OrderCalendarRVAdapter(date_list)
+
+        calRVAdapter.setClickListener(object : OrderCalendarRVAdapter.MyClickListener{
+            override fun itemSelect(posion:Int) {
+
+                initRV()
+            }
+        })
+        binding.orderCalendar.adapter = calRVAdapter
+        binding.orderCalendar.layoutManager = GridLayoutManager(activity, 7)
     }
 
 }
